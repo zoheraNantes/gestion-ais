e9e239f37348a60fdfac0bb2c127536aaa225be0
zelfakhadi2024 <zohera.elfakhadi@gmail.com>
2026-04-27 13:57:33 +0200

    initial commit

---
+++ src/main/java/fr/eni/gestionavis/GestionAvisApplication.java
@@ -0,0 +1,13 @@
+package fr.eni.gestionavis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionAvisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionAvisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/eni/gestionavis/GestionAvisApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.eni.gestionavis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionAvisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
